5db6ff614acb4b79f1aac0c9fe1c0ea0947872d0
w-h-i-t-e-h-a-t-H-k-e-r <82028267+whitehathacker0205@users.noreply.github.com>
2021-05-10 16:46:31 +0800

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,4 +17,35 @@
 	speed: 1000
  });
 
- 	
+ 	var typeText = document.querySelector(".typeText")
+// var textToBeTyped = "a software engineer"
+var textToBeTypedArr = ["a software engineer", "a warlord", "a king", "a peasant"]
+var index = 0, isAdding = true, textToBeTypedIndex = 0
+
+function playAnim() {
+  setTimeout(function () {
+    // set the text of typeText to a substring of the text to be typed using index.
+    typeText.innerText = textToBeTypedArr[textToBeTypedIndex].slice(0, index)
+    if (isAdding) {
+      // adding text
+      if (index > textToBeTypedArr[textToBeTypedIndex].length) {
+        //...
+      } else {
+        //...
+      }
+    } else {
+      // removing text
+      if (index === 0) {
+        //...
+        //switch to next text in text array
+        textToBeTypedIndex = (textToBeTypedIndex + 1) % textToBeTypedArr.length
+      } else {
+        //...
+      }
+    }
+    // calls itself
+    playAnim()
+  }, isAdding ? 120 : 60)
+}
+// start animation
+playAnim()
