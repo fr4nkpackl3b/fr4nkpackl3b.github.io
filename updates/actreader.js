d2c6c6d386c956ef480a4784852a780bebd292f3
w-h-i-t-e-h-a-t-H-k-e-r <82028267+whitehathacker0205@users.noreply.github.com>
2021-05-10 16:57:38 +0800

    updated

--- updates/actreader.js
+++ updates/actreader.js
@@ -16,15 +16,3 @@
  	const scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 1000
  });
-
- 		var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
