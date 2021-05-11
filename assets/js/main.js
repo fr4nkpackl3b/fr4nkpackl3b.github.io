59136dd920967582782ed9fe43db6569ec815721
w-h-i-t-e-h-a-t-H-k-e-r <82028267+whitehathacker0205@users.noreply.github.com>
2021-05-11 08:33:05 +0800

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,18 +17,6 @@
 	speed: 1000
  });
 
- 		var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-
  		var i=0, text;
 	text = "What's New? I added a new project on updates called Web Design."
 	function typing() {
@@ -39,3 +27,57 @@ function typeWriter() {
 		}
 	}
 	typing();
+
+ // typewriter effect
+	const carouselText = [
+  {text: "Hello I'm whitehathacker0205", color: "#8869e4"},
+  {text: "I'm Ariel Fajardo", color: "#17b06b"},
+  {text: "I am whitehathacker0205 or Ariel Fajardo", color: "#ff3c5c"}
+]
+
+$( document ).ready(async function() {
+  carousel(carouselText, "#feature-text")
+});
+
+async function typeSentence(sentence, eleRef, delay = 60) {
+  const letters = sentence.split("");
+  let i = 0;
+  while(i < letters.length) {
+    await waitForMs(delay);
+    $(eleRef).append(letters[i]);
+    i++
+  }
+  return;
+}
+
+async function deleteSentence(eleRef) {
+  const sentence = $(eleRef).html();
+  const letters = sentence.split("");
+  let i = 0;
+  while(letters.length > 0) {
+    await waitForMs(30);
+    letters.pop();
+    $(eleRef).html(letters.join(""));
+  }
+}
+
+async function carousel(carouselList, eleRef) {
+    var i = 0;
+    while(true) {
+      updateFontColor(eleRef, carouselList[i].color)
+      await typeSentence(carouselList[i].text, eleRef);
+      await waitForMs(1500);
+      await deleteSentence(eleRef);
+      await waitForMs(500);
+      i++
+      if(i >= carouselList.length) {i = 0;}
+    }
+}
+
+function updateFontColor(eleRef, color) {
+  $(eleRef).css('color', color);
+}
+
+function waitForMs(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
